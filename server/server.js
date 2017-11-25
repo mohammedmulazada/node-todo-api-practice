e190fe0213eca91da5c975f0412f2ddfb8b63eab
Mohammed Mulazada <thisismoniac@gmail.com>
2017-11-25 14:40:34 +0100

    add patch /todos:id

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const {mongoose} = require('./db/mongoose')
 const {Todo} = require('./models/todo')
 const {User} = require('./models/user')
 const {ObjectID} = require('mongodb')
+const _ = require('lodash')
 
 const port = process.env.PORT || 3000
 
@@ -65,6 +66,31 @@ app.post('/todos', (req, res) => {
 		res.send(doc)
 	}).catch((e) => {
 		res.status(400).send(e)
+	})
+})
+
+app.patch('/todos/:id', (req, res) => {
+	var id = req.params.id
+	var body = _.pick(req.body, ['text', 'completed'])
+
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send()
+	}
+
+	if (_.isBoolean(body.completed) && body.completed) {
+		body.completedAt = new Date().getTime()
+	} else {
+		body.completed = false
+		body.completedAt = null
+	}
+
+	Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+		if (!todo) {
+			return res.status(400).send()
+		}
+		res.send({todo})
+	}).catch((e) => {
+		res.status(400).send()
 	})
 })
 
