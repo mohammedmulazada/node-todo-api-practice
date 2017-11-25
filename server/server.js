f9ae847adee8fb46cf0155e4f94f2fd254f2f000
Mohammed Mulazada <thisismoniac@gmail.com>
2017-11-25 12:43:01 +0100

    Add GET /todos:id

--- server/server.js
+++ server/server.js
@@ -4,10 +4,37 @@ var bodyParser = require('body-parser')
 const {mongoose} = require('./db/mongoose')
 const {Todo} = require('./models/todo')
 const {User} = require('./models/user')
+const {ObjectID} = require('mongodb')
 
 const app = express()
 
 app.use(bodyParser.json())
+
+app.get('/todos', (req, res) => {
+	Todo.find().then((todos) => {
+		res.send({todos})
+	}).catch((e) => {
+		res.status(400).send(e)
+	})
+})
+
+app.get('/todos/:id', (req, res) => {
+	let id = req.params.id
+
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send()
+	}
+
+	Todo.findById(id).then((todo) => {
+		if (!todo) {
+			return res.status(404).send()
+		} 
+		res.status(200).send({todo})
+	}).catch((e) => {
+		res.status(404).send()
+	})
+
+})
 
 app.post('/todos', (req, res) => {
 	var todo = new Todo({
@@ -25,12 +52,6 @@ app.listen(3000, () => {
 	console.log('Started on port 3000')
 })
 
-app.get('/todos', (req, res) => {
-	Todo.find().then((todos) => {
-		res.send({todos})
-	}).catch((e) => {
-		res.status(400).send(e)
-	})
-})
+
 
 module.exports = {app}
