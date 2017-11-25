76ff4f0fddc6394d306711ec2b4e4dd7dfe06e15
Mohammed Mulazada <thisismoniac@gmail.com>
2017-11-25 14:20:40 +0100

    test delete /todos/:id

--- server/server.js
+++ server/server.js
@@ -50,7 +50,7 @@ app.delete('/todos/:id', (req, res) => {
 		if (!todo) {
 			return res.status(404).send()
 		}
-		res.send(todo)
+		res.send({todo})
 	}).catch((e) => {
 		res.status(404).send()
 	})
